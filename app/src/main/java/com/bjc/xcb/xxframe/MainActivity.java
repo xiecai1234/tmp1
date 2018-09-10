f5db60641951c28ad3ce51ca43f147b64e7f6ec2
xiecai1234 <825302814@qq.com>
2018-09-10 22:34:28 +0800

    Initial commit

---
+++ app/src/main/java/com/bjc/xcb/xxframe/MainActivity.java
@@ -0,0 +1,52 @@
+package com.bjc.xcb.xxframe;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    PopupWindow popupWindow;
+    @BindView(R.id.btn_menu)
+    Button btnMenu;
+    @BindView(R.id.activity_main)
+    RelativeLayout activityMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_menu)
+    public void onViewClicked() {
+        // 获取自定义的菜单布局文件
+        View popupWindow_view = getLayoutInflater().inflate(R.layout.pop_menu, null,false);
+        // 创建PopupWindow实例,设置菜单宽度和高度为包裹其自身内容
+        popupWindow = new PopupWindow(popupWindow_view, ActionBar.LayoutParams.WRAP_CONTENT,
+                ActionBar.LayoutParams.WRAP_CONTENT, true);
+        //设置菜单显示在按钮的下面
+        popupWindow.showAsDropDown(findViewById(R.id.btn_menu),0,0);
+        // 点击其他地方消失
+        popupWindow_view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                //如果菜单存在并且为显示状态，就关闭菜单并初始化菜单
+                if (popupWindow != null && popupWindow.isShowing()) {
+                    popupWindow.dismiss();
+                    popupWindow = null;
+                }
+                return false;
+            }
+        });
+    }
+}
